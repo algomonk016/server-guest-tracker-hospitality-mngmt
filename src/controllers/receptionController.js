844fe55fc8c0e2825de52f17eee76a3d057bf262
Shivesh Tiwari <algomonk016@gmail.com>
2021-11-18 09:24:54 +0530

    started reception controller and routes

--- src/controllers/receptionController.js
+++ src/controllers/receptionController.js
@@ -1,6 +1,14 @@
+const connection = require('../db/index')
+
 exports.login = async(req, res, next) => {
     try{
         // login code here
+        const query = 'SELECT * FROM ROOM'
+        connection.query(query, (error, result) => {
+            if(error) throw error
+            console.log('result', result)
+        } )
+
     } catch(error){
         console.log(error)
     }
--- src/routes/customerRoutes.js
+++ src/routes/customerRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const customerController = require('../controllers/customerController')
+const customerController = require('../controllers/customerController');
+const api = require('../utils/API/ApiEndpoints')
 
-router.post('/login', customerController.login)
-router.get('/', customerController.getSomething)
+router.post(api.login, customerController.login)
+router.post(api.feedback, customerController.addFeedback)
 
 module.exports = router
